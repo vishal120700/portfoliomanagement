f794134f121d80bd8e33a0fb6db304b068ca0335
Sandesh Arsud <arsudsandesh97@gmail.com>
2025-03-05 08:47:07 +0530

    ChipS

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,5 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+export const supabaseStorageBucket =
+  process.env.REACT_APP_SUPABASE_STORAGE_BUCKET;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
